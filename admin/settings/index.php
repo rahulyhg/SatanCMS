cdfc5f42c02a0f9c09c9778c60248d9484b115bb
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-21 11:43:21 -0400

    added more inputs

--- admin/settings/index.php
+++ admin/settings/index.php
@@ -1,6 +1,13 @@
 <?php include '../../lib/framework.php';
 echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php");?>
 
-<input type="text" placeholder="Site Title" value="<?=$CMS->parseString('{{Title}}');?>" />
+<input type="text" id="title" placeholder="Site Title" value="<?=$CMS->parseString('{{Title}}');?>" />
+<div class="clear"></div>
+
+<textarea id="meta-description" placeholder="Meta Description"><?=$CMS->parseString('{{meta-description}}');?></textarea>
+<div class="clear"></div>
+
+<input type="text" id="meta-keywords" placeholder="Meta Keywords" value="<?=$CMS->parseString('{{meta-keywords}}');?>" />
+<div class="clear"></div>
 
 <?=$CMS->parse("{$CMS->SITE['DIR']}admin/inc/footer.php"); ?>
