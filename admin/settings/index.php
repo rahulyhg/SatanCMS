1ad398519e8cdcc8c743830a3464b10540bd7dd4
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-21 12:17:55 -0400

    making save, need to go back and finish ajax

--- admin/settings/index.php
+++ admin/settings/index.php
@@ -10,4 +10,25 @@ echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php");?>
 <input type="text" id="meta-keywords" placeholder="Meta Keywords" value="<?=$CMS->parseString('{{meta-keywords}}');?>" />
 <div class="clear"></div>
 
+<button onclick="saveSettings();">Save</button>
+
+<!-- Scripts -->
+<script type="text/javascript">
+function saveSettings(){
+	var title = document.getElementById('title'),
+		description = document.getElementById('meta-description'),
+		keywords = document.getElementById('meta-keywords');
+	ajax('POST',"<?=$CMS->SITE['URL'];?>admin/settings/save.php",
+		{'title': title,
+		'meta-description': description,
+		'meta-keywords': keywords},
+		function(data){
+			var response = JSON.parse(data.reponse);
+			console.log(response);
+		}
+	);
+}
+</script>
+
+
 <?=$CMS->parse("{$CMS->SITE['DIR']}admin/inc/footer.php"); ?>
---
+++ admin/settings/save.php
@@ -0,0 +1,14 @@
+<?php include_once '../../inc/framework.php';
+
+$title = $mysqli->escape_string($_POST['title']);
+$description = $mysqli->escape_string($_POST['meta-description']);
+$keywords = $mysqli->escape_string($_POST['meta-keywords']);
+
+$qry = "INSERT INTO config (`conf_key`,`conf_value`,`conf_datestamp`) VALUES";
+
+$mysqli->query("{$qry} ('title','{$title}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$title}', `conf_datestamp` = UNIX_TIMESTAMP()");
+$mysqli->query("{$qry} ('meta-description','{$description}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$description}', `conf_datestamp` = UNIX_TIMESTAMP()");
+$mysqli->query("{$qry} ('meta-keywords','{$keywords}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$keywords}', `conf_datestamp` = UNIX_TIMESTAMP()");
+
+die();
+?>
