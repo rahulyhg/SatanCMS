e1229e8257a70cba66f7c837d78706d15043a353
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-22 01:16:10 -0400

    styled settings page

--- admin/settings/index.php
+++ admin/settings/index.php
@@ -1,16 +1,21 @@
 <?php include '../../lib/framework.php';
 echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php");?>
 
-<input type="text" id="title" placeholder="Site Title" value="<?=$CMS->parseString('{{Title}}');?>" />
-<div class="clear"></div>
+<div id="settings">
 
-<textarea id="meta-description" placeholder="Meta Description"><?=$CMS->parseString('{{meta-description}}');?></textarea>
-<div class="clear"></div>
+	<input type="text" id="title" placeholder="Site Title" value="<?=$CMS->parseString('{{Title}}');?>" />
+	<div class="clear"></div>
 
-<input type="text" id="meta-keywords" placeholder="Meta Keywords" value="<?=$CMS->parseString('{{meta-keywords}}');?>" />
-<div class="clear"></div>
+	<textarea id="meta-description" placeholder="Meta Description"><?=$CMS->parseString('{{meta-description}}');?></textarea>
+	<div class="clear"></div>
 
-<button onclick="saveSettings();">Save</button>
+	<input type="text" id="meta-keywords" placeholder="Meta Keywords" value="<?=$CMS->parseString('{{meta-keywords}}');?>" />
+	<div class="clear"></div>
+
+	<button onclick="saveSettings();">Save</button>
+	
+</div>
+
 
 <!-- Scripts -->
 <script type="text/javascript">
