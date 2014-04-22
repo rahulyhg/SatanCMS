1928ed14df46208fe112827713a7a1fc21cab2a0
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-22 01:24:50 -0400

    Added header, subheading, and footer

--- admin/settings/index.php
+++ admin/settings/index.php
@@ -12,6 +12,15 @@ echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php");?>
 	<input type="text" id="meta-keywords" placeholder="Meta Keywords" value="<?=$CMS->parseString('{{meta-keywords}}');?>" />
 	<div class="clear"></div>
 
+	<input type="text" id="heading" placeholder="Heading" />
+	<div class="clear"></div>
+
+	<input type="text" id="sub-heading" placeholder="Sub-heading" />
+	<div class="clear"></div>
+
+	<input type="text" id="footer" placeholder="Footer" />
+	<div class="clear"></div>
+
 	<button onclick="saveSettings();">Save</button>
 	
 </div>
@@ -22,11 +31,17 @@ echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php");?>
 function saveSettings(){
 	var title = document.getElementById('title').value,
 		description = document.getElementById('meta-description').value,
-		keywords = document.getElementById('meta-keywords').value;
+		keywords = document.getElementById('meta-keywords').value,
+		heading = document.getElementById('heading').value,
+		subheading = document.getElementById('sub-heading').value,
+		footer = document.getElementById('footer').value;
 	ajax('POST',"<?=$CMS->SITE['URL'];?>admin/settings/save.php",
 		{'title': title,
 		'meta-description': description,
-		'meta-keywords': keywords},
+		'meta-keywords': keywords,
+		'heading': heading,
+		'sub-heading': subheading,
+		'footer': footer},
 		function(data){
 			var response = JSON.parse(data.response);
 			message(response.message,'good');
--- admin/settings/save.php
+++ admin/settings/save.php
@@ -3,12 +3,18 @@
 $title = $mysqli->escape_string($_POST['title']);
 $description = $mysqli->escape_string($_POST['meta-description']);
 $keywords = $mysqli->escape_string($_POST['meta-keywords']);
+$heading = $mysqli->escape_string($_POST['heading']);
+$subheading = $mysqli->escape_string($_POST['sub-heading']);
+$footer = $mysqli->escape_string($_POST['footer']);
 
 $qry = "INSERT INTO config (`conf_key`,`conf_value`,`conf_datestamp`) VALUES";
 
 $mysqli->query("{$qry} ('title','{$title}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$title}', `conf_datestamp` = UNIX_TIMESTAMP()");
 $mysqli->query("{$qry} ('meta-description','{$description}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$description}', `conf_datestamp` = UNIX_TIMESTAMP()");
 $mysqli->query("{$qry} ('meta-keywords','{$keywords}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$keywords}', `conf_datestamp` = UNIX_TIMESTAMP()");
+$mysqli->query("{$qry} ('heading','{$heading}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$heading}', `conf_datestamp` = UNIX_TIMESTAMP()");
+$mysqli->query("{$qry} ('sub-heading','{$subheading}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$subheading}', `conf_datestamp` = UNIX_TIMESTAMP()");
+$mysqli->query("{$qry} ('footer','{$footer}',UNIX_TIMESTAMP()) ON DUPLICATE KEY UPDATE `conf_value` = '{$footer}', `conf_datestamp` = UNIX_TIMESTAMP()");
 
 echo json_encode(array('message' => 'Settings Saved!'));
 
