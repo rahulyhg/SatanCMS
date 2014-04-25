a7007c7524a6d3c5caed56b7072a9c90f01fe75e
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-25 00:12:03 -0400

    button to edit.php

--- admin/media/index.php
+++ admin/media/index.php
@@ -1,4 +1,7 @@
 <?php include '../../lib/framework.php';
 echo $CMS->parse("{$CMS->SITE['DIR']}admin/inc/header.php"); ?>
+<a href="<?=$CMS->SITE['URL'];?>admin/media/edit.php" title="Upload Media" class="header-right"><i class="fa fa-upload"></i></a>
+
+
 
 <?=$CMS->parse("{$CMS->SITE['DIR']}admin/inc/footer.php"); ?>
