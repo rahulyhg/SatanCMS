2cd1caf72af860ad881f4956b2b4b929cd1064e8
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-27 16:16:53 -0400

    added input and styles to media edit page

--- admin/media/edit.php
+++ admin/media/edit.php
@@ -4,16 +4,18 @@ if(isset($_GET['id'])){
 	$id = intval($_GET['id']);
 }else{
 	$id = 0;
-}
-// display title "upload" or "edit"
-if($id){ ?>
-	<h2>Edit Media</h2>
-<?php } else { ?>
-	<h2>Upload Media</h2>
-<?php } ?>
+}?>
 
-
-
+<div id="edit-media">
+	<?php // display title "upload" or "edit"
+	if($id){ ?>
+		<h2>Edit Media</h2>
+	<?php } else { ?>
+		<h2>Upload Media</h2>
+	<?php } ?>
+	
+	<input type="text" placeholder="Title" />
+</div>
 
 
 <?=$CMS->parse("{$CMS->SITE['DIR']}admin/inc/footer.php"); ?>
